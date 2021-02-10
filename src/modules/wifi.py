f989a0bc57380247a2b0795247187f663767108a
Georg Mueksch <georg.mueksch@gmail.com>
2021-02-10 16:49:17 +0100

    use map of wifi credentials, use the strongest wifi

--- src/modules/wifi.py
+++ src/modules/wifi.py
@@ -2,16 +2,46 @@ from config import *
 import network
 
 class Wifi:
-    def do_connect(self, ssid, pwd):
-        import network
-        wlan = network.WLAN(network.STA_IF)
-        wlan.active(True)
-        if not wlan.isconnected():
-            print('connecting to network '+ssid)
-            wlan.connect(ssid,pwd)
-            while not wlan.isconnected():
-                pass
-        print('network config:', wlan.ifconfig())
+    '''
+    Connects to the Wifi defined in config.py
+    Could have multiple ssid configured
+    '''
+    def __init__(self,config_dict):
+        self.config_dict = config_dict
+        self.wlan = network.WLAN(network.STA_IF)
+        self.wlan.active(True)
+        
+    def get_ssid_list_sorted(self):
+        'Strongest wifi first'
+        self.ssid_list = list(map(lambda x: [x[0].decode(),x[3]], self.wlan.scan()) )
+        self.ssid_list.sort(key=lambda y:y[1],reverse=True)
+        return self.ssid_list
+        
+    def do_connect(self,force=False):
+        
+        try:
+            if self.wlan.isconnected() and not force:
+                return True
+            for ssid_pair in self.get_ssid_list_sorted():
+                ssid = ssid_pair[0]
+                pwd = WIFI_CREDS.get(ssid,None)
+                if pwd is None:
+                    continue
+                print("Trying to connect to ",ssid)
+                if not self.wlan.isconnected() or force:
+                    print('connecting to network '+ssid)
+                    self.wlan.connect(ssid,pwd)
+                    while not self.wlan.isconnected():
+                        pass
+                    if force:
+                        force=False
+                    print("connected")
+        except:
+            print('Problem connecting to wifi. Tried: ',WIFI_CREDS.keys())
+            return False
+        finally:
+            print('network config:', self.wlan.ifconfig())
+        
+        
 
-# TODO: Implement scan wifi method to choose the correct password from the config dict
-Wifi().do_connect(list(WIFI_CREDS.keys())[0],list(WIFI_CREDS.values())[0])
+Wifi(WIFI_CREDS).do_connect()
